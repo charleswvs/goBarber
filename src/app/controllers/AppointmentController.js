16928af368bb635e303904492aba9515ef580f42
charles00willian <wcharles15@gmail.com>
2019-11-03 20:20:55 -0300

    Creating appointment listing

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,33 @@
 import * as Yup from 'yup';
-import Appointmet from '../models/Appointment';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmetController {
+  async index(req, res) {
+    const appointment = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointment);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -27,11 +52,38 @@ class AppointmetController {
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
-    const appointment = await Appointmet.create({
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ erro: 'Past dates are not allowed' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailabily = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailabily) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
+
     return res.json(appointment);
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.put('/users', UserController.update); // update user
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 // Uma maneira de testar o envio do meus dados
