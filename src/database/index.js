d42b9f3c34982c03398a6de350dc2b5411366f9c
charles00willian <wcharles15@gmail.com>
2019-11-03 21:35:25 -0300

    Connecting mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -19,6 +21,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndMondify: true }
+    );
   }
 }
 
